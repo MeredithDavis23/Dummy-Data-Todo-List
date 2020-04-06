0730fa02d501adeb5b6ba938b2555a985db78d64
Meredith Davis <meredithd23@yahoo.com>
2020-04-06 13:55:46 -0500

    show incomplete

--- scripts.js
+++ scripts.js
@@ -2,7 +2,12 @@ const element = document.getElementById("todo-list")
 
 let filteredTodos = [];
 
+let completedTodos = [];
+let incompleteTodos = [];
+
+var inputId = Number(document.getElementById("input-id").value);
 var isPopulated = false;
+
 
 let arrayOfTodos = [
     {
@@ -44,11 +49,44 @@ let arrayOfTodos = [
  }
 
  const filterTodos = () => {
+  inputId = Number(document.getElementById("input-id").value);
    if (isPopulated) {
-     const lists = document.querySelectorAll('li')
-     for (let i = 0; i < filteredTodos.length; i++) {
-      element.removeChild(lists[i])
+     element.innerHTML =""
      }
-   }
- }
- 
+    filteredTodos = arrayOfTodos.filter(array => array.userId == inputId)
+    for (let i = 0; i < filteredTodos.length; i++) {
+      let list = document.createElement('li');
+      let text = document.createTextNode(filteredTodos[i].title)
+      list.appendChild(text);
+      if (!filteredTodos[i].completed) {
+        list.classList.add("redText");
+         } else {
+           list.classList.add("greenText");
+         }
+       element.appendChild(list);
+    }
+  }
+
+  const showCompleteTodos = () => {
+       element.innerHTML =""
+      completedTodos = arrayOfTodos.filter(array => array.completed == true)
+      for (let i = 0; i < completedTodos.length; i++) {
+        let list = document.createElement('li');
+        let text = document.createTextNode(completedTodos[i].title)
+        list.appendChild(text);
+             list.classList.add("greenText");
+         element.appendChild(list);
+      }
+    }
+
+    const showIncompleteTodos = () => {
+        element.innerHTML =""
+       incompleteTodos = arrayOfTodos.filter(array => array.completed == false)
+       for (let i = 0; i < incompleteTodos.length; i++) {
+         let list = document.createElement('li');
+         let text = document.createTextNode(incompleteTodos[i].title)
+         list.appendChild(text);
+          list.classList.add("redText");  
+          element.appendChild(list);
+       }
+     }
