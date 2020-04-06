5f99ae671aed3cbc3bc46cbc3fd5a9aab004f371
Meredith Davis <meredithd23@yahoo.com>
2020-04-05 21:57:53 -0500

    change color of text

--- scripts.js
+++ scripts.js
@@ -1,4 +1,12 @@
-const list = document.getElementById("todo-list")
+const element = document.getElementById("todo-list")
+
+// let filteredTodos = [];
+// let filterCompleteTodos = [];
+
+// var isCompleted = true
+// var isNotCompleted = false
+
+// Number(document.getElementById('id-input').value)
 
 let arrayOfTodos = [
     {
@@ -26,31 +34,55 @@ let arrayOfTodos = [
 
  const populateTodos = () => {
    for (let i = 0; i < arrayOfTodos.length; i++) {
-     let element = document.createElement('li')
+     let list = document.createElement('li');
      let text = document.createTextNode(arrayOfTodos[i].title)
-     list.appendChild(element)
-     element.appendChild(text)
-     console.log(element)
-   }
-   if (arrayOfTodos[i].completed==false) {
-    text.element.add(".redText")
-     } else {
-       text.element.add(".greenText")
-     }
+     list.appendChild(text);
+     if (!arrayOfTodos[i].completed) {
+      list.classList.add("redText");
+       } else {
+         list.classList.add("greenText");
+       }
+     element.appendChild(list);
+    }
  }
  
-const filterTodos = () {
-  
-}
+// const filterTodos = () {
+//   idNumber = Number(document.getElementById('id-input').value)
+//     if (idInput > 0 && idInput < 11) {
+//       filteredTodos = arrayOfTodos.filter(array => array.userId == idNumber)
+//       correctInput = true
+//       isFiltered = true
+//     } else { 
+//       correctInput = false 
+//     }
+
+//     if (isPopulated && correctInput) {
+      // const listContent = document
+    // }
  
- 
+// const filterCompleteTodos = isCompleted => {
+//   filterTodos()
+
+//   if (correctInput) {
+//     filterCompleteTodos = filteredTodos.filter(
+//       array => array.completed == isCompleted
+//     )
+//     for (let i = 0; i < filterCompleteTodos.length; i++) {
+//       if (!isCompleted) {
+//         element.style.setProperty('color', 'red')
+//       }
+//       element.appendChild(document.createTextNode(filterCompleteTodos[i].title))
+//       list.append(element)
+//     }
+//   }
+// }
  
   // const populateTodos = () => {
   //    for (let i = 0; i < arrayOfTodos.length; i++) {
   //     let listItem = document.createElement('li');
   //     let textNode = document.createTextNode(arrayOfTodos[i].title);
   //     listItem.appendChild(textNode);
-  //     let element = document.getElementById("todo-list");
+  //     let element = document.getElementById('todo-list');
   //     element.appendChild(listItem);
   //     if (arrayOfTodos[i].completed ==false) {
   //       listItem.classList.add(".redText")
